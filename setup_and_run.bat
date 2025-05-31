0723927d708bcd0b6c27dba3d83829a79a5156f5
Dzunisani Phaweni <dzunisaniphaweni@Dzunisanis-MacBook-Pro.local>
2025-05-31 19:49:50 +0200

    windows exe updates

--- setup_and_run.bat
+++ setup_and_run.bat
@@ -1,25 +1,41 @@
 @echo off
 cd /d %~dp0
 set VENV_DIR=venv
+set PYTHON_EXE=%VENV_DIR%\Scripts\python.exe
 
 REM Check if virtual environment exists
-if not exist %VENV_DIR%\Scripts\activate.bat (
+if not exist %PYTHON_EXE% (
     echo Creating virtual environment...
     python -m venv %VENV_DIR%
+
+    REM Activate the environment (cmd-specific)
+    call %VENV_DIR%\Scripts\activate.bat
+
+    REM Display the Python path and version
+    echo === Python being used ===
+    echo %PYTHON_EXE%
+    %PYTHON_EXE% --version
+
+    REM Install dependencies
+    echo === Installing dependencies... ===
+    %PYTHON_EXE% -m pip install --upgrade pip
+    %PYTHON_EXE% -m pip install aiohttp pandas plotly shinywidgets tqdm scp
+
+) else (
+    REM Activate the environment (cmd-specific)
+    call %VENV_DIR%\Scripts\activate.bat
+
+    REM Display the Python path and version
+    echo === Python being used ===
+    echo %PYTHON_EXE%
+    %PYTHON_EXE% --version
+
+    echo Dependencies installation skipped since virtual environment exists.
 )
 
-REM Activate the environment
-call %VENV_DIR%\Scripts\activate.bat
-
-REM Install dependencies
-echo Installing dependencies...
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python -m pip install --force-reinstall "plotly[express]"
-
 REM Run the app
-echo Launching the app in your browser...
-python app.py
+echo === Launching the app in your browser... ===
+%PYTHON_EXE% app.py
 
 REM Pause to keep window open if the script crashes
 pause
